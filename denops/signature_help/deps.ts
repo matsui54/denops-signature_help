d2aaef4b95ae8c6ebe34c0cb3c38d6f9f4a5b213
matsui54 <matsui54@users.noreply.github.com>
2022-04-08 01:45:00 +0000

    :package: Update Deno dependencies
    Update dependencies by udd:
    
    /home/runner/work/denops-signature_help/denops-signature_help/denops/signature_help/config.ts
    
    /home/runner/work/denops-signature_help/denops-signature_help/denops/signature_help/integ.ts
    
    /home/runner/work/denops-signature_help/denops-signature_help/denops/signature_help/signature.ts
    
    /home/runner/work/denops-signature_help/denops-signature_help/denops/signature_help/markdown.ts
    
    /home/runner/work/denops-signature_help/denops-signature_help/denops/signature_help/event.ts
    
    /home/runner/work/denops-signature_help/denops-signature_help/denops/signature_help/main.ts
    
    /home/runner/work/denops-signature_help/denops-signature_help/denops/signature_help/test.ts
    
    /home/runner/work/denops-signature_help/denops-signature_help/denops/signature_help/markdown_test.ts
    
    /home/runner/work/denops-signature_help/denops-signature_help/denops/signature_help/types.ts
    
    /home/runner/work/denops-signature_help/denops-signature_help/denops/signature_help/deps.ts
    [1/10] Looking for releases: https://deno.land/x/denops_std@v3.3.0/mod.ts
    [1/10] Using latest: https://deno.land/x/denops_std@v3.3.0/mod.ts
    [2/10] Looking for releases: https://deno.land/x/denops_std@v3.3.0/batch/mod.ts
    [2/10] Using latest: https://deno.land/x/denops_std@v3.3.0/batch/mod.ts
    [3/10] Looking for releases: https://deno.land/x/denops_std@v3.3.0/option/mod.ts
    [3/10] Using latest: https://deno.land/x/denops_std@v3.3.0/option/mod.ts
    [4/10] Looking for releases: https://deno.land/x/denops_std@v3.3.0/function/mod.ts
    [4/10] Using latest: https://deno.land/x/denops_std@v3.3.0/function/mod.ts
    [5/10] Looking for releases: https://deno.land/x/denops_std@v3.3.0/function/nvim/mod.ts
    [5/10] Using latest: https://deno.land/x/denops_std@v3.3.0/function/nvim/mod.ts
    [6/10] Looking for releases: https://deno.land/x/denops_std@v3.3.0/variable/mod.ts
    [6/10] Using latest: https://deno.land/x/denops_std@v3.3.0/variable/mod.ts
    [7/10] Looking for releases: https://deno.land/x/denops_std@v3.3.0/autocmd/mod.ts
    [7/10] Using latest: https://deno.land/x/denops_std@v3.3.0/autocmd/mod.ts
    [8/10] Looking for releases: https://deno.land/std@0.133.0/testing/asserts.ts
    [8/10] Attempting update: https://deno.land/std@0.133.0/testing/asserts.ts -> 0.134.0
    [8/10] Update successful: https://deno.land/std@0.133.0/testing/asserts.ts -> 0.134.0
    [9/10] Looking for releases: https://deno.land/std@0.133.0/log/mod.ts
    [9/10] Attempting update: https://deno.land/std@0.133.0/log/mod.ts -> 0.134.0
    [9/10] Update successful: https://deno.land/std@0.133.0/log/mod.ts -> 0.134.0
    [10/10] Looking for releases: https://deno.land/x/unknownutil@v2.0.0/mod.ts
    [10/10] Using latest: https://deno.land/x/unknownutil@v2.0.0/mod.ts
    
    /home/runner/work/denops-signature_help/denops-signature_help/README.md
    
    Already latest version:
    https://deno.land/x/denops_std@v3.3.0/mod.ts == v3.3.0
    https://deno.land/x/denops_std@v3.3.0/batch/mod.ts == v3.3.0
    https://deno.land/x/denops_std@v3.3.0/option/mod.ts == v3.3.0
    https://deno.land/x/denops_std@v3.3.0/function/mod.ts == v3.3.0
    https://deno.land/x/denops_std@v3.3.0/function/nvim/mod.ts == v3.3.0
    https://deno.land/x/denops_std@v3.3.0/variable/mod.ts == v3.3.0
    https://deno.land/x/denops_std@v3.3.0/autocmd/mod.ts == v3.3.0
    https://deno.land/x/unknownutil@v2.0.0/mod.ts == v2.0.0
    
    Successfully updated:
    https://deno.land/std@0.133.0/testing/asserts.ts 0.133.0 -> 0.134.0
    https://deno.land/std@0.133.0/log/mod.ts 0.133.0 -> 0.134.0
    make[1]: Entering directory '/home/runner/work/denops-signature_help/denops-signature_help'
    make[1]: Leaving directory '/home/runner/work/denops-signature_help/denops-signature_help'

--- denops/signature_help/deps.ts
+++ denops/signature_help/deps.ts
@@ -8,6 +8,6 @@ export * as fn from "https://deno.land/x/denops_std@v3.3.0/function/mod.ts";
 export * as nvimFn from "https://deno.land/x/denops_std@v3.3.0/function/nvim/mod.ts";
 export * as vars from "https://deno.land/x/denops_std@v3.3.0/variable/mod.ts";
 export * as autocmd from "https://deno.land/x/denops_std@v3.3.0/autocmd/mod.ts";
-export { assertEquals } from "https://deno.land/std@0.133.0/testing/asserts.ts";
-export * as log from "https://deno.land/std@0.133.0/log/mod.ts";
+export { assertEquals } from "https://deno.land/std@0.134.0/testing/asserts.ts";
+export * as log from "https://deno.land/std@0.134.0/log/mod.ts";
 export { isLike } from "https://deno.land/x/unknownutil@v2.0.0/mod.ts";
