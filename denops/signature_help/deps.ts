854df53f0b2c96a83f97872b071ffa18aacf15cf
2357 <61523777+ryota2357@users.noreply.github.com>
2023-08-17 09:39:02 +0900

    📦 Update Deno dependencies

--- denops/signature_help/deps.ts
+++ denops/signature_help/deps.ts
@@ -8,5 +8,5 @@ export * as fn from "https://deno.land/x/denops_std@v5.0.1/function/mod.ts";
 export * as nvimFn from "https://deno.land/x/denops_std@v5.0.1/function/nvim/mod.ts";
 export * as vars from "https://deno.land/x/denops_std@v5.0.1/variable/mod.ts";
 export * as autocmd from "https://deno.land/x/denops_std@v5.0.1/autocmd/mod.ts";
-export { assertEquals } from "https://deno.land/std@0.173.0/testing/asserts.ts";
-export * as log from "https://deno.land/std@0.173.0/log/mod.ts";
+export { assertEquals } from "https://deno.land/std@0.198.0/testing/asserts.ts";
+export * as log from "https://deno.land/std@0.198.0/log/mod.ts";
